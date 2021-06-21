5424e75bd5c5a0b70e86d2b08e1e4175812262f6
rajivranjanmars <rajivranjanmars@gmail.com>
2021-06-21 07:19:11 +0530

    https://leetcode.com/problems/remove-element/

---
+++ removeElement.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+  int removeElement(vector<int>nums, int val) {
+        for (int i = nums.size()-1; i >=0; i--){
+            if (nums[i] == val){
+               nums.erase(nums.begin() + i);
+            }
+         }
+         return nums.size();
+     }
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, x;
+    cin >> t;
+    vector<int> nums;
+    while (t--){
+        cin >> x;
+        nums.push_back(x);
+    }
+    cin >> x;
+      cout << removeElement(nums,x) ;
+    return 0;
+}
