edb927c8026f10154379549d95b6d1c46a0be938
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2022-06-05 04:39:28 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (97.40%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+      int solve(int x,int n,int col,int diag,int alt_diag){
+        if(x==n)
+            return 1;
+         int res=0;
+        for(int y=0;y<n;y++){
+            
+            if(!(col&(1<<y))&&!(diag&(1<<(x+y)))&&!(alt_diag&(1<<(x-y+n-1)))){
+               res+=(solve(x+1,n,col|(1<<y),diag|(1<<(x+y)),alt_diag|(1<<(x-y+n-1))));
+            }
+        }
+        return res;
+    }
+    
+    int totalNQueens(int n) {
+      int col=0,diag=0,alt_diag=0;
+        return solve(0,n,col,diag,alt_diag);   
+    }
+};
