5357df35bfef5b1a28c9a9357bd4bf470c004ded
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2022-07-09 09:22:18 +0530

    Added solution - LeetHub

---
+++ 14. Boolean Matrix - GFG/14.-boolean-matrix.cpp
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(vector<vector<int> > &matrix)
+    {
+        // code here 
+        
+        
+       int r = matrix.size();
+       int c = matrix[0].size();
+       vector<int> onesPos;
+       
+       for(int i = 0; i < r; i++){
+           for(int j = 0; j < c; j++){
+               if(matrix[i][j] == 1){
+                   onesPos.push_back(i);
+                   onesPos.push_back(j);
+               }
+           }
+       }
+       
+       int n = onesPos.size();
+       for(int i = 0; i < n; i += 2){
+           int row = onesPos[i];
+           int col = onesPos[i + 1];
+           
+           for(int j = 0; j < c; j++)
+               matrix[row][j] = 1;
+               
+           for(int j = 0; j < r; j++)
+               matrix[j][col] = 1;
+           
+       }
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        ob.booleanMatrix(matrix);
+
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+
+
+  // } Driver Code Ends
