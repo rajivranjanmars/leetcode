dda7a34f0ec2b09a19081baaacdc62d2feb3803a
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2022-07-09 17:03:45 +0530

    Added solution - LeetHub

--- 27. LRU Cache - GFG/27.-lru-cache.cpp
+++ 27. LRU Cache - GFG/27.-lru-cache.cpp
@@ -8,84 +8,93 @@ using namespace std;
 
 class LRUCache
 {
-   private:
-   struct Node 
-   {
-       int key;
-       int value;
-       Node* prev;
-       Node* next;
-       Node(int key1, int value1)
-       {
-           key = key1;
-           value = value1;
-       }
-   };
-   Node* head = new Node(0,0);
-   Node* tail = new Node(0,0);  
-   
-   unordered_map<int,Node*> map1;
-   int capacity;
-   public:
-   //Constructor for initializing the cache capacity with the given value.
-   LRUCache(int cap)
-   {
-       capacity = cap;
-       head->next = tail;
-       tail->prev = head;
-   }
-   
-   //Function to return value corresponding to the key.
-   int get(int key)
-   {
-       if(map1.find(key) != map1.end())
-       {
-           Node* existingNode = map1[key];
-           int value = existingNode->value;
-           deleteNode(existingNode);
-           addNode(key,value);
-           map1.erase(key);
-           map1[key] = head->next;
-           return value;
-       }
-       return -1;
-   }
-   
-   //Function for storing key-value pair.
-   void set(int key, int value)
-   {
-       if(map1.find(key) != map1.end())
-       {
-           Node* existingNode = map1[key];
-           map1.erase(key);
-           deleteNode(existingNode);
-       }
-       
-       if(map1.size() == capacity)
-       {
-           map1.erase(tail->prev->key);
-           deleteNode(tail->prev);
-       }
-       
-       addNode(key,value);
-       map1[key]=head->next;
+      public:
+    /*creating a doubly ll that contains the key, value pair as the data*/
+    class Node{
+        public:
+            int key;
+            int val;
+            Node *prev;
+            Node *next;
+            Node(int k, int v){
+                key = k;
+                val = v;
+            }
+    };
+    
+    /*Giving the, key and value to head and tail as -1,-1*/
+    Node *head = new Node(-1, -1);
+    Node *tail = new Node(-1, -1);
+    int size;
+    unordered_map<int, Node *> m;    /*in this map we store the key and the pointer
+                                    to the node associated with it*/
+                                    
+    //public:
+    //Constructor for initializing the cache capacity with the given value.
+    LRUCache(int cap)
+    {
+        // code here
+        size = cap;
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    void addnode(Node *new_node){
+        Node *temp = head->next;
+        new_node->next = temp;
+        new_node->prev = head;
+        head->next = new_node;
+        temp->prev = new_node;
+    }
+    
+    void deletenode(Node * delnode){
+        Node *del_node_next = delnode->next;
+        Node *del_node_prev = delnode->prev;
+        del_node_prev->next = del_node_next;
+        del_node_next->prev = del_node_prev;
+    }
+    
+    
+    //Function to return value corresponding to the key.
+    int get(int key)
+    {
+        // your code here
+        int a;
+        if(m.find(key)!=m.end()){       //key exists in map
+            Node *add_of_node = m[key];     /*m[key] returns the value of its counterpair
+                                        (in this case, address ofthe node associated with it)*/
+            a = add_of_node->val;
+            m.erase(key);
+            deletenode(add_of_node);
+            addnode(add_of_node);
+            m[key] = head->next;
+        }else{
+            a = -1;
+        }
+        return a;
+    }
+    
+    //Function for storing key-value pair.
+    void set(int key, int value)
+    {
+        // your code here
+        if(m.find(key)!=m.end()){
+            Node *add_of_node = m[key];
+            add_of_node->val = value;
+            deletenode(add_of_node);
+            addnode(add_of_node);
+            m[key] = head->next;
+        }else{
+            if(m.size() == size){
+                m.erase((tail->prev)->key);
+                deletenode(tail->prev);
+            }
+            Node *newnode = new Node(key, value);
+            addnode(newnode);
+            m.insert(pair<int, Node*>(key, newnode));
+        }
+    }
 
-   }
-   void addNode(int key,int value)
-   {
-       Node* createNode = new Node(key,value);
-       createNode->next = head->next;
-       head->next->prev = createNode;
-       head->next = createNode;
-       createNode->prev=head;
-   }
-   
-   void deleteNode(Node* deletedNode)
-   {
-       deletedNode->prev->next = deletedNode->next;
-       deletedNode->next->prev = deletedNode->prev;
-       delete deletedNode;
-   }
 };
 
 // { Driver Code Starts.
