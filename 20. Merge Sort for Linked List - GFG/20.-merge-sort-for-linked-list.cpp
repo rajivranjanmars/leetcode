79d810d89a598737752ed251064fbb999dca5aff
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2022-07-09 16:45:47 +0530

    Added solution - LeetHub

---
+++ 20. Merge Sort for Linked List - GFG/20.-merge-sort-for-linked-list.cpp
@@ -0,0 +1,90 @@
+// { Driver Code Starts
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+/* Structure of the linked list node is as
+struct Node 
+{
+    int data;
+    struct Node* next;
+    Node(int x) { data = x;  next = NULL; }
+};
+*/
+
+
+class Solution{
+  public:
+    //Function to sort the given linked list using Merge Sort.
+    Node* mergeSort(Node* head) {
+        // your code here
+         Node* temp=head;
+        vector<int>arr;
+        int n=0;
+        while(temp!=NULL)
+        {
+            arr.push_back(temp->data);
+            temp=temp->next;
+            n++;
+        }
+        sort(arr.begin(),arr.end());
+        temp=head;
+        int i=0;
+        while(temp!=NULL)
+        {
+            temp->data=arr[i];
+            i++;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
+
+
+// { Driver Code Starts.
+
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+void push(struct Node** head_ref, int new_data) {
+    Node* new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+int main() {
+    long test;
+    cin >> test;
+    while (test--) {
+        struct Node* a = NULL;
+        long n, tmp;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> tmp;
+            push(&a, tmp);
+        }
+        Solution obj;
+        a = obj.mergeSort(a);
+        printList(a);
+    }
+    return 0;
+}  // } Driver Code Ends
