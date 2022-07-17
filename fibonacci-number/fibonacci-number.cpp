f2da44798b6e502bb9a41f0eb8351e7fd149aab4
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2022-07-17 23:24:23 +0530

    Time: 3 ms, Memory: 6.2 MB - LeetHub

---
+++ fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        vector<int>v;
+        v.push_back(0);
+        v.push_back(1);
+        for (int i=2;i<=n;i++){
+            v.push_back(v[i-1]+v[i-2]);
+        }
+        return v[n];
+    }
+};
