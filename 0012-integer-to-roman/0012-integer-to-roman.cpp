21304a787ac04d52831cd1a0d4bf19e97967a5b1
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2023-05-06 11:45:37 +0530

    Time: 35 ms (5.39%), Space: 12.7 MB (7.40%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     string intToRoman(int num) {
-         vector<int> values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        vector<string> numerals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+          map<int, string> romanTable = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
         string result = "";
-        for(int i=0; i<values.size(); i++){
-            while(num >= values[i]){
-                result += numerals[i];
-                num -= values[i];
+        for (auto it = romanTable.rbegin(); it != romanTable.rend(); ++it) {
+            while (num >= it->first) {
+                num -= it->first;
+                result += it->second;
             }
         }
         return result;
