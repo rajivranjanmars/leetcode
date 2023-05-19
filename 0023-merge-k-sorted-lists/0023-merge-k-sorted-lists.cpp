7605ac6922996092520f481912fbc5847f72da74
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2023-05-19 17:17:00 +0530

    Time: 24 ms (73.59%), Space: 13.9 MB (26.66%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,19 +11,35 @@
 
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int>v;
-        for(int i=0;i<lists.size();i++){
-            while(lists[i]){
-                v.push_back(lists[i]->val);
-                lists[i]=lists[i]->next;
-            }
+   ListNode* mergeKLists(vector<ListNode*>& lists) {
+    int k = lists.size(); // Get the number of linked lists
+    if (k == 0)
+        return NULL;
+
+    vector<pair<int, ListNode*>> arr; // Create a vector pair to store values and nodes
+
+    for (int i = 0; i < k; i++) {
+        ListNode* curr_list = lists[i]; // Extract the current linked list
+
+        while (curr_list != NULL) { // Traverse the linked list
+            arr.push_back({curr_list -> val, curr_list}); // Store value and node in the vector pair
+            curr_list = curr_list -> next; // Move to the next node
         }
-        sort(rbegin(v),rend(v));
-        ListNode* ans=nullptr;
-        for(int i=0;i<v.size();i++){
-            ans=new ListNode(v[i],ans);
-        }
-        return ans;
     }
+
+    if (arr.size() == 0)
+        return NULL;
+
+    sort(arr.begin(), arr.end()); // Sort the vector pair based on the values
+
+    for (int i = 0; i < arr.size() - 1; i++) {
+        arr[i].second -> next = arr[i + 1].second; // Link the nodes together
+    }
+
+    arr[arr.size() - 1].second -> next = NULL; // Set the next pointer of the last node to NULL
+
+    return arr[0].second; // Return the head of the merged linked list
+}
+
+
 };
