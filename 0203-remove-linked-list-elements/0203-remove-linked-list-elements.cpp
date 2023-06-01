ed3b10697283bc7a9a3df6331c1ff9a72ec0ac3f
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2023-06-01 09:57:25 +0530

    Time: 30 ms (22.65%), Space: 14.8 MB (94.76%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -11,21 +11,18 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        if(head==nullptr) return head;
+         if(head==nullptr) return head;
+		
         while(head!=nullptr && head->val==val){
             head = head->next;
         }
         ListNode* curr = head;
-        ListNode* prev = nullptr;
-        while(curr!=nullptr){
-            if(curr->val==val){
-                prev->next = curr->next;
+        while(curr!=nullptr  && curr->next!=nullptr){
+            if(curr->next->val==val){
+                curr->next = curr->next->next;
+            }
+            else
                 curr = curr->next;
-            }
-            else{
-                prev = curr;
-                curr = curr->next;        
-            }
         }
         return head;
     }
